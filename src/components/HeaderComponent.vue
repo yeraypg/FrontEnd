c7ea188f517693836ae3890673c73580841faa18
yeraypg <yeraypg@gmail.com>
2022-10-17 22:37:06 +0100

    add header and footer components

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-app-bar app color="rgb(106, 162, 236">
+        <div class="d-flex align-center">
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
+          />
+          <v-img
+            alt="Vuetify Name"
+            class="shrink mt-1 hidden-sm-and-down"
+            contain
+            min-width="100"
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+            width="100"
+          />
+        </div>
+        <v-spacer></v-spacer>
+      </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+
+  data: () => ({})
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,29 +14,3 @@ export default {
   }
 }
 </script>
-<style scoped>
-.background {
-  width: 100vw;
-  height: 87vh;
-  background: radial-gradient(
-      farthest-side at top left,
-      rgba(104, 164, 243, 0.5),
-      transparent
-    ),
-    radial-gradient(
-      farthest-side at top right,
-      rgba(140, 253, 109, 0.5),
-      transparent
-    ),
-    radial-gradient(
-      farthest-side at bottom left,
-      rgba(248, 132, 78, 0.5),
-      transparent
-    ),
-    radial-gradient(
-      farthest-corner at bottom right,
-      rgba(68, 68, 68, 0.5),
-      transparent
-    );
-}
-</style>

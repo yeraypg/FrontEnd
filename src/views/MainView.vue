1f588d09f5cf0dc13a70cb2b617c0847cc67e5c3
yeraypg <yeraypg@gmail.com>
2022-10-19 10:54:10 +0100

    added buttons on main view

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,12 +1,29 @@
 <template>
-  <div>
+  <v-container>
     <h1>MAIN PAGE</h1>
-  </div>
+    <v-spacer></v-spacer>
+    <v-btn x-large color="success" dark @click="goSpot">SPOT VIEW </v-btn>
+
+    <v-btn x-large color="success" dark @click="goFlop">FLOP VIEW </v-btn>
+
+    <v-btn x-large color="success" dark>EXAMPLE VIEW</v-btn>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'MainView',
-  components: {}
+  components: {},
+  methods: {
+    goSpot: function () {
+      this.$router.push({ name: 'spot' })
+    },
+    goFlop: function () {
+      this.$router.push({ name: 'flop' })
+    },
+    goExample: function () {
+      this.$router.push({ name: 'example' })
+    }
+  }
 }
 </script>
--- src/views/SpotView.vue
+++ src/views/SpotView.vue
@@ -1,12 +1,21 @@
 <template>
-    <div>
-        <h1>SPOT PAGE</h1>
-    </div>
+  <v-container>
+    <h1>SPOT PAGE</h1>
+    <v-spacer></v-spacer>
+    <v-btn x-large color="success" dark @click="goMain"
+      >BACK TO MAIN VIEW
+    </v-btn>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'SpotView',
-  components: {}
+  components: {},
+  methods: {
+    goMain: function () {
+      this.$router.push({ name: 'main' })
+    }
+  }
 }
 </script>

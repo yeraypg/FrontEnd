7fe28289d72007091cf17c6e39b427f98c09a594
yeraypg <yeraypg@gmail.com>
2022-10-19 18:28:05 +0100

    create branchs

---
+++ src/components/SpotCardComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card>
+    <v-card-title>Spot Card</v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'SpotCardComponent',
+
+  data: () => ({})
+}
+</script>
--- src/views/SpotView.vue
+++ src/views/SpotView.vue
@@ -5,13 +5,15 @@
     <v-btn x-large color="success" dark @click="goMain"
       >BACK TO MAIN VIEW
     </v-btn>
+    <SpotCardComponent />
   </v-container>
 </template>
 
 <script>
+import SpotCardComponent from '../components/SpotCardComponent.vue'
 export default {
   name: 'SpotView',
-  components: {},
+  components: { SpotCardComponent },
   methods: {
     goMain: function () {
       this.$router.push({ name: 'main' })

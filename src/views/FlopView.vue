51fe5e3c54a050112092f4997abbbd9fb3a9d0ae
adrian <profilte82@gmail.com>
2022-10-19 20:56:52 +0100

    First commit Flop

--- src/views/FlopView.vue
+++ src/views/FlopView.vue
@@ -1,12 +1,29 @@
 <template>
-    <div>
-        <h1>FLOP PAGE</h1>
-    </div>
+  <div>
+    <h1>FLOP PAGE</h1>
+    <v-btn x-large color="success" dark @click="goMain"
+      >BACK TO MAIN VIEW
+    </v-btn>
+    <v-btn x-large color="success" dark @click="goSpot"
+      >BACK TO SPOT VIEW
+    </v-btn>
+    <FlopCardComponent />
+  </div>
 </template>
 
 <script>
+import FlopCardComponent from '../components/FlopCardComponent.vue'
+
 export default {
   name: 'FlopView',
-  components: {}
+  components: { FlopCardComponent },
+  methods: {
+    goMain: function () {
+      this.$router.push({ name: 'main' })
+    },
+    goSpot: function () {
+      this.$router.push({ name: 'spot' })
+    }
+  }
 }
 </script>

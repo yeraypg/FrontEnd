d3ff348bfbd1fdb4a401911ee51d09fc231f6169
adrian <profilte82@gmail.com>
2022-10-20 20:44:06 +0100

    FlopView first template

---
+++ src/components/FlopCardText.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card class="mr-auto my-auto" width="95%" height="100%">
+    <v-card-text
+      >Hola que tal esto es un prueba para ver el funcionamoento de vue, esto es
+      una pasada y tengo hambre, estoy quemado y quiero jugar a la playstation
+      5</v-card-text
+    >
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'FlopCardtext',
+
+  data: () => ({})
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/FlopView.vue
+++ src/views/FlopView.vue
@@ -1,12 +1,45 @@
 <template>
-    <div>
-        <h1>FLOP PAGE</h1>
-    </div>
+  <v-container>
+    <h1>FLOP PAGE</h1>
+    <v-row>
+      <v-col cols="3">
+        <v-btn x-large color="success" dark @click="goMain"
+          >BACK TO MAIN VIEW
+        </v-btn>
+      </v-col>
+      <v-col cols="3">
+        <v-btn x-large color="success" dark @click="goSpot"
+          >BACK TO SPOT VIEW
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-spacer></v-spacer>
+      <v-col cols="6">
+        <FlopCardText />
+      </v-col>
+      <v-col cols="6">
+        <FlopCardImg />
+      </v-col>
+      <v-spacer></v-spacer>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import FlopCardImg from '../components/FlopCardImg.vue'
+import FlopCardText from '../components/FlopCardText.vue'
+
 export default {
   name: 'FlopView',
-  components: {}
+  components: { FlopCardImg, FlopCardText },
+  methods: {
+    goMain: function () {
+      this.$router.push({ name: 'main' })
+    },
+    goSpot: function () {
+      this.$router.push({ name: 'spot' })
+    }
+  }
 }
 </script>

0dc8d57c9bc12d7dd046cddaa4ae6308d65ef56e
yeraypg <yeraypg@gmail.com>
2022-10-20 09:48:26 +0100

    add select filter in main view

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,8 +1,20 @@
 <template>
   <v-container>
-    <h1>MAIN PAGE</h1>
+    <v-row>
+      <v-col cols="4">
+        <h1>MAIN PAGE</h1>
+      </v-col>
+      <v-spacer></v-spacer>
+      <v-col cols="4">
+        <SelectTypeSpotComponent @changeSelectFilter="chageSelectFilter" />
+      </v-col>
+    </v-row>
     <v-container class="cardRows">
-      <SpotCardComponent v-for="spot in userSpots" :key="spot.id" :spot="spot"/>
+      <SpotCardComponent
+        v-for="spot in getFilteredSpots"
+        :key="spot.id"
+        :spot="spot"
+      />
     </v-container>
   </v-container>
 </template>
@@ -10,15 +22,17 @@
 <script>
 import SpotCardComponent from '../components/SpotCardComponent.vue'
 import { getAllSpots } from '../services/spotService'
+import SelectTypeSpotComponent from '../components/SelectTypeSpotComponent.vue'
 
 export default {
   name: 'MainView',
   data () {
     return {
-      userSpots: []
+      userSpots: [],
+      typeSelect: 'SRP'
     }
   },
-  components: { SpotCardComponent },
+  components: { SpotCardComponent, SelectTypeSpotComponent },
   methods: {
     goMain: function () {
       this.$router.push({ name: 'main' })
@@ -26,6 +40,15 @@ export default {
     getAllSpots: async function () {
       const data = await getAllSpots(localStorage.userId)
       this.userSpots = data
+    },
+    chageSelectFilter: function (type) {
+      console.log(type)
+      this.typeSelect = type
+    }
+  },
+  computed: {
+    getFilteredSpots: function () {
+      return this.userSpots.filter((spot) => spot.type === this.typeSelect)
     }
   },
   created () {
@@ -34,7 +57,7 @@ export default {
 }
 </script>
 <style scoped>
-.cardRows{
+.cardRows {
   display: flex;
 }
 </style>
